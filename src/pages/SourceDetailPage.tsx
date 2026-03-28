8116290ee3ef7dded9bfa950802ce4ceed0362f0
Meterion Ops <meterion.ops@gmail.com>
2026-03-28 12:01:10 +0200

    Update SourceDetailPage.tsx

--- src/pages/SourceDetailPage.tsx
+++ src/pages/SourceDetailPage.tsx
@@ -5,3 +5,10 @@ const USE_MOCK_EXECUTION = false;
 
 const execution = USE_MOCK_EXECUTION ? mockExecutionSurface : null;
 <ExecutionStatusCard execution={execution} />
+import { DecisionHistoryCard } from "../components/source-detail/DecisionHistoryCard";
+import { ExecutionHistoryCard } from "../components/source-detail/ExecutionHistoryCard";
+import { CaseTimelineCard } from "../components/source-detail/CaseTimelineCard";
+
+import { mockDecisionHistorySurface } from "../dev/mockDecisionHistorySurface";
+import { mockExecutionHistorySurface } from "../dev/mockExecutionHistorySurface";
+import { mockCaseTimelineSurface } from "../dev/mockCaseTimelineSurface";
