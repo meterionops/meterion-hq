a06840d3bf13783fa1eec714850633aa17ed907c
Meterion Ops <meterion.ops@gmail.com>
2026-09-24 06:56:23 +0300

    web-acquisition: add src/meterion_web_acquisition/__init__.py

---
+++ packages/web-acquisition/src/meterion_web_acquisition/__init__.py
@@ -0,0 +1,46 @@
+from .engine import CollectionEngine, EngineRequest, EngineResponse
+from .evidence import build_evidence, sha256_bytes, utc_now_iso
+from .jev import (
+    COLLECTION_STRATEGY_INSTRUCTION,
+    COLLECTION_STRATEGY_OPTIONS,
+    JevDecisionProvider,
+    collection_strategy_state,
+)
+from .models import (
+    CandidateObservation,
+    CanonicalDecision,
+    CollectionJob,
+    EvidenceRecord,
+    RoutingDecision,
+    SourceObservation,
+    SourceProfile,
+)
+from .pipeline import AcquisitionRun, run_acquisition
+from .policy import route_collection
+from .validation import ValidationResult, deterministic_decision, validate_candidate
+
+__all__ = [
+    "AcquisitionRun",
+    "CandidateObservation",
+    "CanonicalDecision",
+    "CollectionEngine",
+    "CollectionJob",
+    "EngineRequest",
+    "EngineResponse",
+    "EvidenceRecord",
+    "RoutingDecision",
+    "SourceObservation",
+    "SourceProfile",
+    "ValidationResult",
+    "JevDecisionProvider",
+    "COLLECTION_STRATEGY_INSTRUCTION",
+    "COLLECTION_STRATEGY_OPTIONS",
+    "build_evidence",
+    "collection_strategy_state",
+    "deterministic_decision",
+    "route_collection",
+    "run_acquisition",
+    "sha256_bytes",
+    "utc_now_iso",
+    "validate_candidate",
+]
